fc1f34984fa5908143ee271a642c1ae8da4abf37
thecodervaibhav <101354104+thecodervaibhav@users.noreply.github.com>
2022-05-07 00:46:02 +0530

    Add files via upload

---
+++ navbar/navbar_js/navbar.js
@@ -0,0 +1,14 @@
+function show()
+{
+    // console.log("hii")
+    var showme=document.querySelector("#moreopt")
+    // showme.setAttribute("display","block")
+    showme.style.display="block";
+}
+
+function showsign()
+{
+    var showme=document.querySelector("#sing_in_button")
+    // showme.setAttribute("display","block")
+    showme.style.display="block";
+}
