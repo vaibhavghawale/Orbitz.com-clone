ab03194e0e8f50d60e7cc81cfd6c6033fb91b489
raje2 <100850727+raje2@users.noreply.github.com>
2022-05-07 22:35:31 +0530

    support page update

---
+++ All_CSS/support.js
@@ -0,0 +1,8 @@
+document.querySelector("#button1").addEventListener("click",btnFunction);
+document.querySelector("#button_list").addEventListener("click", listFunction);
+function listFunction(){
+    window.location.href="https://apps.expediapartnercentral.com/en_US/list?siteId=70201&tpid=70201&eapid=0&langId=1033"
+}
+function btnFunction(){
+    window.location.href="https://www.orbitz.com/rewards/"
+}
